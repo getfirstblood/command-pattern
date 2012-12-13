dc118086706d25fa19779e5cc8a22c5ca14819d4
gaoweiwei <getfirstblood@gmail.com>
2012-12-13 21:01:37 +0800

    Initial Commit

---
+++ command pattern/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  command pattern
+//
+//  Created by gaoweiwei on 12-12-13.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
